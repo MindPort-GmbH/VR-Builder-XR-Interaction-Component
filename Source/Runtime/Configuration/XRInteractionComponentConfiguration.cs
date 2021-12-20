da1e906f7c1f02089113e32aac2a0e7d2ab64590
Marcello Tridenti <marcello.tridenti@tutanota.com>
2021-12-20 15:35:50 +0100

    Added component config

---
+++ Source/Runtime/Configuration/XRInteractionComponentConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using VRBuilder.Core.Configuration;
+
+namespace VRBuilder.XRInteraction.Configuration
+{
+    public class XRInteractionComponentConfiguration : IInteractionComponentConfiguration
+    {
+        public string DisplayName => "XR Interaction Component";
+
+        public bool IsXRInteractionComponent => true;
+    }
+}
